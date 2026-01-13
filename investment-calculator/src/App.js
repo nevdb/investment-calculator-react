37801387d72958ae998dbd472b1cdc6e9e553279
nevena <dnevena27@gmail.com>
2026-01-13 12:12:52 +0200

    Results table

--- investment-calculator/src/App.js
+++ investment-calculator/src/App.js
@@ -1,13 +1,33 @@
+import { useState } from 'react';
+
 import './index.css';
 import Header from './components/Header.jsx';
 import UserInput from './components/UserInput.jsx';
+import Results from './components/Results.jsx';
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
   return (
     <div className="App">
       <>
         <Header />
-        <UserInput />
+        <UserInput userInput={userInput} onChangeInput={handleChange} />
+        <Results input={userInput} />
       </>
 
     </div>
