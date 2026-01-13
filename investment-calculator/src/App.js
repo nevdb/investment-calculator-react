4023e431826e8e56b0331750cdda5b3cd0418758
nevena <dnevena27@gmail.com>
2026-01-13 12:35:58 +0200

    render conditionally results, add more style

--- investment-calculator/src/App.js
+++ investment-calculator/src/App.js
@@ -13,6 +13,8 @@ function App() {
     duration: 10,
   });
 
+  const inputIsValid = userInput.duration >= 1;
+
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
       return {
@@ -27,7 +29,11 @@ function App() {
       <>
         <Header />
         <UserInput userInput={userInput} onChangeInput={handleChange} />
-        <Results input={userInput} />
+        {
+          inputIsValid ? <Results input={userInput} /> : <p>Please enter a period greater than zero!</p>
+        }
+
+
       </>
 
     </div>
